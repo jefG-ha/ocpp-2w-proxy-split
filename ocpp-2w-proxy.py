63d706934bec2ca00c676e507e8cece42d485045
jens-markussen <jens.markussen@gmail.com>
2025-05-18 09:48:58 +0200

    Saving work

--- ocpp-2w-proxy.py
+++ ocpp-2w-proxy.py
@@ -3,51 +3,135 @@
 
 import asyncio
 import logging
-import signal
 
+import websockets
 import websockets.asyncio
 import websockets.asyncio.server
-import websockets.asyncio.client
-import http
-import websockets
+from websockets.exceptions import ConnectionClosed, ConnectionClosedError
+from websockets.frames import CloseCode
+
 import ssl
 from pathlib import Path
 import argparse
-
-"""
-from ocpp.v16.enums import (
-    AuthorizationStatus,
-    ChargingProfileKindType,
-    ChargingProfilePurposeType,
-    ChargingRateUnitType,
-    CiStringType,
-    HashAlgorithm,
-    Location,
-    Measurand,
-    Phase,
-    ReadingContext,
-    RecurrencyKind,
-    UnitOfMeasure,
-    ValueFormat,
-)
-from ocpp.v16.datatypes import (
-    ChargingProfile
-)
-from ocpp.v16.call import (
-    SetChargingProfile
-) 
-"""
+import configparser
 
 __version__ = "0.1.0"
 
-iso15118_certs = None
+config = configparser.ConfigParser()
 
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger("OCPP2WProxy")
 
-global url
+class OCPP2WProxy: # Forward declaration
+    pass
 
-# Track active connections by client path
-active_client_connections = {}
+# main class
+class OCPP2WProxy:
+    # Static dict of OCPP2WProxy instances. key is charger_id
+    proxy_list: dict[OCPP2WProxy] = {}
+
+    def __init__(self, websocket: websockets.server.WebSocketServerProtocol, charger_id: str):
+        # Store the websocket for later     
+        logger.debug(websocket.request)
+        self.ws = websocket
+        self.charger_id = charger_id
+
+        # Chech that charger id looks reasonable
+        if not charger_id.isalnum():
+            logger.error(f"Charger ID '{charger_id}' is not alphanumeric")
+            raise Exception("Charger ID is not alphanumeric")
+
+        # Initialize table of CSMS call ids sent to the charger in order to respond back 
+        # (only) to the CSMS that issued the call
+        self.call_ids: dict[str, str] = {}
+
+        # Insert new OCPP2WProxy instance in the (static) dict of instances.
+        self.proxy_list[charger_id] = self
+
+    async def close(self):
+        """Close all connections to the charger and primary, secondary server"""
+        await self.ws.close()
+        await self.primary_connection.close()
+        if self.secondary_connection:
+            await self.secondary_connection.close()
+
+    async def check_delete_old(charger_id: str):
+        """Check if there are any old instances of this charger in the proxy list"""
+        if charger_id in OCPP2WProxy.proxy_list:
+            logger.info(f"Charger ID {self.charger_id} already exists. Closing and deleting")
+            proxy = OCPP2WProxy.proxy_list[charger_id]
+            await proxy.close()
+            del OCPP2WProxy.proxy_list[charger_id]
+
+    async def run(self):
+        """Main loop for this proxy. This is where all the magic happens."""
+
+        # Create connections to the two CSMSes.
+        # Forward any available Authorization and User-Agent headers
+        headers = {}
+        if "Authorization" in self.ws.request.headers:
+            headers["Authorization"] = self.ws.request.headers["Authorization"]
+            logger.debug(f'Authorization header set to {headers["Authorization"]}')
+        user_agent = self.ws.request.headers.get("User-Agent", None) 
+        subprotocols = self.ws.request.headers.get("Sec-WebSocket-Protocol", ["ocpp1.6"])
+        primary_url = config.primary_server + "/" + self.charger_id
+        if config.secondary_server:
+            secondary_url = config.secondary_server + "/" + self.charger_id
+
+        try:
+            self.primary_connection = await websockets.connect(
+                uri=primary_url,
+                user_agent_header=user_agent,
+                additional_headers=headers,
+                subprotocols=subprotocols,
+            )
+            logger.info(f"Connected to primary server @ {primary_url}")
+
+            # Connect to secondary server if it is enabled.
+            if config.secondary_server:
+                self.secondary_connection = await websockets.connect(
+                    uri=secondary_url,
+                    user_agent_header=user_agent,
+                    additional_headers=headers,
+                    subprotocols=subprotocols,
+                )
+                logger.info(f"Connected to secondary server @ {secondary_url}")
+            else:
+                self.secondary_connection = None
+                logger.info("Secondary server not enabled")
+
+        except Exception as e:
+            logger.error(f"Failed to connected to upstream server(s): {e}")
+            return await self.ws.close(CloseCode.ABNORMAL_CLOSURE, "Failed to connect to upstream server(s)")
+        
+        # Create tasks to handle the charger. Each task each to handle receiving messages from
+        # the charger, and the (one or two) CSMSes, and finally a watch dog task to take down
+        # connections if connection goes stale.
+        self.tasks = []
+        self.tasks.append(asyncio.create_task(self.receive_charger_messages()))
+        self.tasks.append(asyncio.create_task(self.receive_primary_messages()))
+        if self.secondary_connection is not None:
+            self.tasks.append(asyncio.create_task(self.receive_secondary_messages()))
+        self.tasks.append(asyncio.create_task(self.watchdog()))
+
+        # Wait for tasks to complete
+        done, pending = await asyncio.wait(self.tasks, return_when=asyncio.FIRST_COMPLETED)
+        logger.debug(f"Task(s) completed for {self.charger_id}: {done}, {pending}")
+
+        for task in done:
+            e = task.exception()
+            if e:
+                logger.warning(f"{self.charger_id} (Not serious - likely connection loss) Task {task} raised exception {e} related to charger ")
+
+        # Cancel any remaining tasks
+        for task in pending:
+            task.cancel()
+
+
+    
+
+
+
 
 # Forward traffic from server (portal).
 async def forward_from_server(client_ws, server_ws, path):
@@ -55,7 +139,7 @@ async def forward_from_server(client_ws, server_ws, path):
         async for message in server_ws:
             logging.info("v[" + path + "] " + message)
             await client_ws.send(message)
-    except websockets.ConnectionClosed:
+    except ConnectionClosed:
         pass
 
 # Forward traffic from client (charger).
@@ -64,23 +148,38 @@ async def forward_from_client(client_ws, server_ws, path):
         async for message in client_ws:
             logging.info("^[" + path + "] " + message)
             await server_ws.send(message)
-    except websockets.ConnectionClosed:
+    except ConnectionClosed:
         pass
 
 # Connection handler (charger connects)
-async def handler(client_ws):
-    logging.debug('Connection request')
-    logging.debug(client_ws.request)
-    path = client_ws.request.path
-    charge_point_id = path.strip("/")
-    logging.info(f'Connection from {charge_point_id}')
+async def on_connect(websocket: websockets.asyncio.server.ServerConnection):
+    logger.debug('Connection request')
+    # Determine charger_id (final part of path)
+    path = websocket.request.path
+    charger_id = path.strip("/")
+    logger.info(f'Connection from {charger_id}')
 
-    # Close any existing connection with the same client_path
-    if path in active_client_connections:
-        old_ws = active_client_connections(path)
-        logging.error(f'Found previous client connection for {path}, closing old.')
-        await old_ws.close(reason="New connection established")
-    active_client_connections[path] = client_ws
+    try:
+        # Delete any existing charger setup
+        await OCPP2WProxy.delete_charger_setup(charger_id)
+
+        # Setup
+        proxy = OCPP2WProxy(websocket=websocket, charger_id=charger_id)
+        await proxy.setup(
+
+        # Connect and run proxy operations
+        await proxy.run()
+
+    except Exception as e:
+        logger.error(f'Error creating OCPP2WProxy. {e}')
+    finally:
+        logger.info("Connection closed.")
+
+
+
+
+
+
 
     # Establish ws/wss connection to URL.
     portal_uri = url + "/" + charge_point_id
@@ -88,11 +187,12 @@ async def handler(client_ws):
     requested_protocols = ['ocpp1.6']   
     logging.info(f'Establishing connection to upstream URL @ {portal_uri}. Protocol is {requested_protocols}')
 
+
     try:
         async with websockets.connect(uri = portal_uri, subprotocols=requested_protocols, ssl = True) as server_ws:
             # Two tasks: forward data both ways
-            forward_server_to_client = await asyncio.create_task(forward_from_server(client_ws, server_ws, path))
-            forward_client_to_server = await asyncio.create_task(forward_from_client(client_ws, server_ws, path))
+            forward_server_to_client = await asyncio.create_task(forward_from_server(websocket, server_ws, path))
+            forward_client_to_server = await asyncio.create_task(forward_from_client(websocket, server_ws, path))
 
             # Wait for both tasks to complete
             done, pending = await asyncio.wait(
@@ -125,38 +225,21 @@ async def handler(client_ws):
 # Main. Decode arguments, setup handler
 async def main():
     parser = argparse.ArgumentParser(
-        description='A simple websocket proxy')
+        description='ocpp-2w-proxy: A two way OCPP proxy')
     parser.add_argument('--version', action='version',
                         version=f'%(prog)s {__version__}')
-
-    parser.add_argument('--host', type=str, default="0.0.0.0",
-                        help='Host to listen on (default: 0.0.0.0)')
-
-    parser.add_argument('--port', type=int, default=9999,
-                        help='Plaintext port to listen on (default: 9999)')
-
-    parser.add_argument('--tls-host', type=str, default=None,
-                        help='TLS Host to listen on (default: value of --host (0.0.0.0))')
-
-    parser.add_argument('--tls-port', type=int, default=9901,
-                        help='TLS port to listen on (default: 9901)')
-
-    parser.add_argument('--cert-chain', type=str, default=None,
-                        help='Certificate chain to be used with TLS websockets. If not provided TLS will be disabled')
-
-    parser.add_argument('--certs', type=str, default="/var/",
-                        help='Directory containing certificates (default: ../everest-core/build/dist/etc/everest/certs)')
-
-    parser.add_argument('certificates', type=str, default=None, nargs='?',
-                        help='Directory containing certificates (default: identical to --certs')
-
-    parser.add_argument('--url', type=str, default="ws://localhost:9000",
-                        help='Websocket URL to connect to, ws:// or wss://')
-
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="ocpp2-2w-proxy.ini",
+        help="Configuration file (INI format). Default ocpp-2w-proxy.ini",
+    )
     args = parser.parse_args()
 
-    global url
-    url = args.url
+    # Read config. config object is then available (via config import) to all.
+    logger.warning(f"Reading config from {args.config}")
+    config.read(args.config)
+
     if not url:
         logging.error("No --url argument supplied. Exiting.")
         return
@@ -171,37 +254,17 @@ async def main():
 
     cert_chain = args.cert_chain
 
-    certs = Path(args.certs)
-    if not certs.exists():
-        logging.warning(
-            'Directory containing certificates does not exist, ISO15118 features are not available')
-    else:
-        global iso15118_certs
-        iso15118_certs = certs
-
     tls_server = None
     if cert_chain:
         ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
         ssl_context.load_cert_chain(cert_chain)
         server = await websockets.serve(
-            handler, tls_host, tls_port,subprotocols=["ocpp1.6", "ocpp2.0.1"], ssl=ssl_context
+            on_connect, tls_host, tls_port,subprotocols=["ocpp1.6", "ocpp2.0.1"], ssl=ssl_context
         )
     else:
         server = await websockets.serve(
-            handler, host, port, subprotocols=["ocpp1.6", "ocpp2.0.1"]
+            on_connect, host, port, subprotocols=["ocpp1.6", "ocpp2.0.1"]
         )
-
-    # Function to shut down the server gracefully
-    def shutdown():
-        print("Shutting down server...")
-        server.close()
-        asyncio.create_task(server.wait_closed())
-        loop.stop()
-
-    # Register signal handlers for SIGINT (Ctrl+C) and SIGTERM (Docker stop)
-    loop = asyncio.get_running_loop()
-    for sig in (signal.SIGINT, signal.SIGTERM):
-        loop.add_signal_handler(sig, shutdown)
 
     logging.info("Proxy ready. Waiting for new connections...")
     await server.wait_closed()
